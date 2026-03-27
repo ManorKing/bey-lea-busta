8b26d385d0ad057c538bfc6c10af2666d19e5c1b
ManorKing <themanorkings@gmail.com>
2026-03-27 11:13:15 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,26 @@
-const CACHE_NAME = 'bey-lea-busta-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['/','/index.html'])));
+const CACHE_NAME = 'autocaddy-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Install the service worker and cache the app shell
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+// Serve cached content when offline or on bad cell service
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
